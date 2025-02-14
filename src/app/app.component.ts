9c8b1f18efeecd081272f4d34dd1285108085d07
Vitor Lima <vitornascimento923@gmail.com>
2025-02-14 02:41:48 -0300

    feat(components): add Input component with styles and tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IconComponent } from "./core/shared/components/icon/icon.component";
 import { ButtonComponent } from "./core/shared/components/button/button.component";
+import { InputComponent } from "./core/shared/components/input/input.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, IconComponent, ButtonComponent],
+  imports: [RouterOutlet, IconComponent, ButtonComponent, InputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/core/shared/components/input/input.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  imports: [CommonModule],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.scss'
+})
+export class InputComponent {
+  @Input() placeholder: string = "Digite aqui...";
+  @Input() type: string = 'default';
+  @Input() size: 'sm' | 'md' | 'lg' = 'md';
+  @Input() disabled: boolean = false;
+
+}
