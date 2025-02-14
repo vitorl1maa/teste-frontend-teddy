f585bedc52b32bc0d082fcc15628a31722e80afa
Vitor Lima <vitornascimento923@gmail.com>
2025-02-14 13:44:21 -0300

    feat(components): implement IconComponent with dynamic icon rendering and styling

--- src/app/core/shared/components/icon/icon.component.ts
+++ src/app/core/shared/components/icon/icon.component.ts
@@ -1,11 +1,85 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { LucideAngularModule, Plus, Minus, Trash2, Pencil, X, AlignJustify } from 'lucide-angular';
+import { IconType } from './types/icon.type';
 
 @Component({
   selector: 'app-icon',
-  imports: [],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './icon.component.html',
   styleUrl: './icon.component.scss'
 })
-export class IconComponent {
+export class IconComponent implements OnChanges {
+  @Input() icon: "minus" | "trash2" | "pencil" | "x" | "alignJustify" | "plus" = "plus"
+  @Input() size: 'sm' | 'md' | 'lg' = 'md';
+  @Input() color: "primary" | "secondary" | "dangerous" = "primary"
+
+
+  lucideIcon: IconType = Plus
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['icon']) {
+
+      this.currentIcon()
+    }
+  }
+
+  currentIcon() {
+    switch (this.icon) {
+      case "minus":
+        this.lucideIcon = Minus;
+        break;
+
+      case "pencil":
+        this.lucideIcon = Pencil;
+        break;
+
+      case "trash2":
+        this.lucideIcon = Trash2;
+        break;
+
+      case "x":
+        this.lucideIcon = X;
+        break;
+
+      case "alignJustify":
+        this.lucideIcon = AlignJustify;
+        break;
+
+      default:
+        this.lucideIcon = Plus;
+        break;
+    }
+  }
+
+  get iconSize(): string {
+    switch (this.size) {
+      case "sm":
+        return "16";
+
+      case "md":
+        return "32";
+
+      case "lg":
+        return "40";
+
+      default:
+        return "24";
+    }
+  }
+
+
+  get iconColor() {
+    switch (this.color) {
+      case "secondary":
+        return "#EC6724"
+
+      case "dangerous":
+        return "#ff0000"
+
+      default:
+        return "#000000"
+    }
+  }
 
 }
---
+++ src/app/core/shared/components/icon/types/icon.type.ts
@@ -0,0 +1,9 @@
+import { Plus, Minus, Trash2, Pencil, X, AlignJustify } from "lucide-angular";
+
+export type IconType =
+  | typeof Plus
+  | typeof Minus
+  | typeof Trash2
+  | typeof Pencil
+  | typeof X
+  | typeof AlignJustify;
