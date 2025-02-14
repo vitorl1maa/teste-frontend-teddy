dbfc7de2abc17aa0e1ccf79406b21e36aef57e8b
Vitor Lima <vitornascimento923@gmail.com>
2025-02-14 13:44:36 -0300

    feat(navbar): enhance navbar layout and add user greeting fix(navbar): update logo path and improve menu styling fix(clients-page): pass menu items to Navbar component fix(input): rename default style to text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { RegistrationPageComponent } from "./pages/registration-page/registration-page.component";
 import { ClientsPageComponent } from "./pages/clients-page/clients-page.component";
 import { NavbarComponent } from "./core/shared/components/navbar/navbar.component";
+import { IconComponent } from "./core/shared/components/icon/icon.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RegistrationPageComponent, ClientsPageComponent, NavbarComponent],
+  imports: [RouterOutlet, RegistrationPageComponent, ClientsPageComponent, NavbarComponent, IconComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
