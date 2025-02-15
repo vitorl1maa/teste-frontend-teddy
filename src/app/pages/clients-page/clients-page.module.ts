509d5a7977b9dd0aac7a3ad41effaabf2eb9d432
Vitor Lima <vitornascimento923@gmail.com>
2025-02-15 15:07:07 -0300

    feat(pages): add routing and module setup for clients and registration pages

---
+++ src/app/pages/clients-page/clients-page.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClientsPageRoutingModule } from './clients-page-routing.module';
+import { ClientsPageComponent } from './clients-page.component';
+import { LayoutComponent } from "../../core/shared/layout/layout/layout.component";
+import { ClientsListComponent } from "./components/clients-list/clients-list.component";
+import { ModalCreatComponent } from "./components/modal-actions/modal-creat/modal-creat.component";
+import { ButtonComponent } from "../../core/shared/components/button/button.component";
+import { ModalEditComponent } from "./components/modal-actions/modal-edit/modal-edit.component";
+import { ModalDeleteComponent } from "./components/modal-actions/modal-delete/modal-delete.component";
+
+
+@NgModule({
+  declarations: [ClientsPageComponent],
+  imports: [
+    CommonModule,
+    ClientsPageRoutingModule,
+    LayoutComponent,
+    ClientsListComponent,
+    ModalCreatComponent,
+    ButtonComponent,
+    ModalEditComponent,
+    ModalDeleteComponent
+  ]
+})
+export class ClientsPageModule { }
--- src/app/pages/registration-page/registration-page.module.ts
+++ src/app/pages/registration-page/registration-page.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RegistrationPageComponent } from './registration-page.component';
 import { RegistrationPageRoutingModule } from './registration-page-routing.module';
+import { ButtonComponent } from '../../core/shared/components/button/button.component';
+import { InputComponent } from '../../core/shared/components/input/input.component';
+import { RegistrationPageComponent } from './registration-page.component';
 
 
-
-// @NgModule({
-//   declarations: [RegistrationPageComponent],
-//   imports: [
-//     CommonModule,
-//     RegistrationPageRoutingModule,
-//   ]
-// })
-// export class RegistrationPageModule { }
+@NgModule({
+  declarations: [RegistrationPageComponent],
+  imports: [
+    CommonModule,
+    RegistrationPageRoutingModule,
+    InputComponent,
+    ButtonComponent
+  ]
+})
+export class RegistrationPageModule { }
