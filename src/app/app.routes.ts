df478f35ce25a6f068afba3aa6ebc8c6205fa4a5
Vitor Lima <vitornascimento923@gmail.com>
2025-02-15 15:06:33 -0300

    feat(routes): add routing configuration for registration and clients pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', loadChildren: () =>
+      import('./pages/registration-page/registration-page.module')
+        .then(m => m.RegistrationPageModule)
+  },
+  {
+    path: 'clientes', loadChildren: () =>
+      import('./pages/clients-page/clients-page.module')
+        .then(m => m.ClientsPageModule)
+  },
+  { path: '**', redirectTo: '' }
+];
