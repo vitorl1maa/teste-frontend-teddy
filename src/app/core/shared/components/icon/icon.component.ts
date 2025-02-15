5772b8fc14ec9cd99e0a29d7b9f15e91a24a2159
Vitor Lima <vitornascimento923@gmail.com>
2025-02-15 11:23:46 -0300

    feat(icon): add 'check' icon option to IconComponent

--- src/app/core/shared/components/icon/icon.component.ts
+++ src/app/core/shared/components/icon/icon.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { LucideAngularModule, Plus, Minus, Trash2, Pencil, X, AlignJustify, ArrowLeft } from 'lucide-angular';
+import { LucideAngularModule, Plus, Minus, Trash2, Pencil, X, AlignJustify, ArrowLeft, Check } from 'lucide-angular';
 import { IconType } from './types/icon.type';
 
 @Component({
@@ -10,7 +10,7 @@ import { IconType } from './types/icon.type';
   styleUrl: './icon.component.scss'
 })
 export class IconComponent implements OnChanges {
-  @Input() icon: "minus" | "trash2" | "pencil" | "x" | "alignJustify" | "arrowLeft" | "plus" = "plus"
+  @Input() icon: "minus" | "trash2" | "pencil" | "x" | "alignJustify" | "arrowLeft" | "check" | "plus" = "plus"
   @Input() size: 'sm' | 'md' | 'lg' = 'md';
   @Input() color: "primary" | "secondary" | "dangerous" = "primary"
 
@@ -48,6 +48,11 @@ export class IconComponent implements OnChanges {
 
       case "arrowLeft":
         this.lucideIcon = ArrowLeft;
+        break;
+
+
+      case "check":
+        this.lucideIcon = Check;
         break;
 
       default:
