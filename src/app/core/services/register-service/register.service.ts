a6b8fc91ec9375a14d05c3553f26c75a1fb94cf1
Vitor Lima <vitornascimento923@gmail.com>
2025-02-17 12:36:29 -0300

    feat(service): add register service

---
+++ src/app/core/services/register-service/register.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { UserCookieEnum } from '../../enum/user.enum';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CookieRegisterUser {
+
+  constructor(private cookieService: CookieService) { }
+
+  setUserName(value: string, days: number = 7) {
+    const expires = new Date();
+    expires.setDate(expires.getDate() + days);
+    this.cookieService.set(UserCookieEnum.USER_NAME, JSON.stringify(value), expires);
+  }
+
+  getUserName(): string {
+    const item = this.cookieService.get(UserCookieEnum.USER_NAME);
+    return item ? JSON.parse(item) : 'Usuário';
+  }
+
+  deleteUserName() {
+    this.cookieService.delete(UserCookieEnum.USER_NAME);
+  }
+}
